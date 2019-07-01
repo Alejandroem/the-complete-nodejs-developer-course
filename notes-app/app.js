af44be87e22ff0aecfd95e6a200e4cec6307c886
Alejandro <alexalejandroem@gmail.com>
2019-07-01 13:09:17 +0200

    Added add remove list and read commands with yarg

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,17 +2,47 @@ const chalk = require('chalk');
 const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-/*
-const command = process.argv[2];
-
-if(command === 'add'){
-    console.log('Adding note!');
-} else if (command === 'remove'){
-    console.log('Removing note!');
-}
-*/
-
-//Customize yargs version
+// Customize yargs version
 yargs.version('1.1.0');
 
-console.log(yargs.argv);
+// Create add command
+yargs.command({
+   command: 'add',
+   describe: 'Add a new note',
+   handler: function(){
+       console.log("Adding a new note!");
+   }
+});
+
+// Create remove command
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function (){
+        console.log('Removing the note');
+    }
+});
+
+// Create list command
+
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function (){
+        console.log('List the notes');
+    }
+});
+
+// Create read command
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function (){
+        console.log('Read a note');
+    }
+});
+
+// add, remove, read, list
+console.log(yargs.argv)
