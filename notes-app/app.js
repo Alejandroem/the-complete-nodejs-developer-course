b58c15b849aec6ae4eee603a7f2adf364edeb0c7
Alejandro <alexalejandroem@gmail.com>
2019-07-01 13:03:56 +0200

    yargs version customization

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,8 @@
-const getNotes = require('./notes.js');
 const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
 
+/*
 const command = process.argv[2];
 
 if(command === 'add'){
@@ -8,3 +10,9 @@ if(command === 'add'){
 } else if (command === 'remove'){
     console.log('Removing note!');
 }
+*/
+
+//Customize yargs version
+yargs.version('1.1.0');
+
+console.log(yargs.argv);
