abe48942ad9f1a0115a38f0daf33327860a9ce47
Alejandro <alexalejandroem@gmail.com>
2019-07-01 12:26:32 +0200

    chalk npm package challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,9 @@
-const validator = require('validator');
 const getNotes = require('./notes.js');
+const chalk = require('chalk');
 
 const msg = getNotes();
 
 console.log(msg);
 
-console.log(validator.isEmail('gmail.com'));
+console.log(chalk.green.bold.inverse('Success'));
+
