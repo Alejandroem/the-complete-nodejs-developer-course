3f85baef25f751f3af37a2ca3d8c95d33a43b7d1
Alejandro <alexalejandroem@gmail.com>
2019-07-01 13:27:44 +0200

    read json file from filesystem

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
 
-const book = {
-    title : 'Ego is the Enemy',
-    author: 'Ryan Holiday'
-};
+// const book = {
+//     title : 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// };
+//
+// const bookJSON = JSON.stringify(book);
+//
+// console.log(bookJSON);
+//
+// const parsedData = JSON.parse(bookJSON);
+//
+// console.log(parsedData.author);
+//
+// fs.writeFileSync('playground/1-json.json',bookJSON);
 
-const bookJSON = JSON.stringify(book);
 
-console.log(bookJSON);
-
-const parsedData = JSON.parse(bookJSON);
-
-console.log(parsedData.author);
-
-fs.writeFileSync('playground/1-json.json',bookJSON);
+const dataBuffer = fs.readFileSync('1-json.json');
+const data = JSON.parse(dataBuffer.toString());
+console.log(data.title);
