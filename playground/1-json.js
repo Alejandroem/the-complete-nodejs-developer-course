7838758d5b31caf8c073ed30282ec10bcbcb2c5a
Alejandro <alexalejandroem@gmail.com>
2019-07-01 13:33:45 +0200

    JSON Challenge

--- playground/1-json.js
+++ playground/1-json.js
@@ -16,6 +16,20 @@ const fs = require('fs');
 // fs.writeFileSync('playground/1-json.json',bookJSON);
 
 
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const data = JSON.parse(dataBuffer.toString());
+// console.log(data.title);
+
 const dataBuffer = fs.readFileSync('1-json.json');
-const data = JSON.parse(dataBuffer.toString());
-console.log(data.title);
+let data = JSON.parse(dataBuffer.toString());
+console.log(data);
+data.name = 'Alex';
+data.age = 26;
+
+const dataJSON = JSON.stringify(data);
+fs.writeFileSync('1-json.json',dataJSON);
+
+
+
+
+
